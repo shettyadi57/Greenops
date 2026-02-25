36abde6e7c344aa2beddcca5e6c558646465ae1f
Adithya S Shetty <shettyadithyas57@gmail.com>
2026-02-25 23:06:29 +0530

    final project

--- script.js
+++ script.js
@@ -12,3 +12,21 @@
             });
         });
     
+function downloadAgent() {
+    const os = navigator.platform.toLowerCase();
+
+    if (os.includes('win')) {
+        window.location.href = "downloads/GreenOps-Windows.exe";
+    } 
+    else if (os.includes('mac')) {
+        window.location.href = "downloads/GreenOps-macOS.zip";
+    } 
+    else {
+        window.location.href = "downloads/GreenOps-Linux";
+    }
+}
+
+
+<button onclick="downloadAgent()" class="os-btn">
+    Download for Your OS
+</button>
